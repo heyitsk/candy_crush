81357f3b57209d5708827221acf5e9b065c4309d
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-11-09 01:43:40 +0530

    candy slide added

--- logic.js
+++ logic.js
@@ -10,8 +10,8 @@ window.onload = () => {
 
     window.setInterval(function(){
         crushCandy();
-        // slideCandy();
-        // generateCandy();
+        slideCandy();
+        generateCandy();
     }, 100);
     
 }
@@ -243,3 +243,20 @@ function checkvalid(){
     }
     return false;
 }
+function slideCandy() {
+    for (let c = 0; c < colums; c++) {
+        let ind = rows - 1;
+        for (let r = colums-1; r >= 0; r--) {
+            if (!board[r][c].src.includes("blank")) {
+                board[ind][c].src = board[r][c].src;
+                ind -= 1;
+            }
+        }
+
+        for (let r = ind; r >= 0; r--) {
+            board[r][c].src = "./images/blank.png";
+        }
+    }
+}
+
+
